63dfa937aa0c5cf003e1cd9d07f3e4d7e963762c
Vladislav Vlasenko <you@example.comvlad-vlasenko@mail.ru>
2022-07-24 19:05:41 +0300

    'scroll'

--- js/script.js
+++ js/script.js
@@ -98,36 +98,47 @@ window.addEventListener('DOMContentLoaded', function() {
     //------------------------------------------------------------Modal---------------------------------------
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]');
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]');
 
-    modalTrigger.forEach(btn => {
-        btn.addEventListener('click', function() {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            // Либо вариант с toggle - но тогда назначить класс в верстке
-            document.body.style.overflow = 'hidden';
-        });
-    });
+modalTrigger.forEach(btn => {
+    btn.addEventListener('click', openModal);
+});
 
-    function closeModal() {
-        modal.classList.add('hide');
-        modal.classList.remove('show');
-        // Либо вариант с toggle - но тогда назначить класс в верстке
-        document.body.style.overflow = '';
+function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+}
+
+function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+}
+
+modalCloseBtn.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        closeModal();
     }
-    
-    modalCloseBtn.addEventListener('click', closeModal);
+});
 
-    modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
-            closeModal();
-        }
-    });
+document.addEventListener('keydown', (e) => {
+    if (e.code === "Escape" && modal.classList.contains('show')) { 
+        closeModal();
+    }
+});
 
-    document.addEventListener('keydown', (e) => {
-        if (e.code === "Escape" && modal.classList.contains('show')) { 
-            closeModal();
-        }
-    });
+const modalTimerId = setTimeout(openModal, 3000);
+
+function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        openModal();
+        window.removeEventListener('scroll', showModalByScroll);
+    }
+}
+window.addEventListener('scroll', showModalByScroll);
 });
